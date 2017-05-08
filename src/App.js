30c1078fa661f3a0fb0eea2d86a07ad1a823a3b1
RhanCandia <rhancandia@gmail.com>
2017-05-08 22:23:36 +0800

    init: webpack2-react-es6 boilerplate

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>App works, yay!</h1>
+  );
+};
+
+export default App;
---
+++ webpack.production.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const copyWebpackPlugin = new CopyWebpackPlugin([
+  {
+    from: './public'
+  }
+], {
+  ignore: [
+    'index.html'
+  ]
+});
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'assets/js/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    htmlWebpackPlugin,
+    copyWebpackPlugin,
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+}
