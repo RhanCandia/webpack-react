054e0e06c77f80b8730d5f5b7a23af0e8810a0c5
RhanCandia <rhancandia@gmail.com>
2017-05-09 16:26:54 +0800

    added http-server now uses single webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+let config = {
   entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "static/js/bundle.js"
+  },
   module: {
     rules: [
       {
@@ -24,9 +28,11 @@ module.exports = {
     compress: true,
     historyApiFallback: true,
     hot: true,
+    open: true,
     https: false,
     noInfo: true
   },
+  devtool: "eval-source-map",
   plugins: [
     new htmlWebpackPlugin({
       template: './src/index.html',
@@ -34,8 +40,26 @@ module.exports = {
       inject: 'body'
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"',
-    }),
-    new webpack.HotModuleReplacementPlugin()
+      'process.env.NODE_ENV': JSON.stringify((process.env.PRODUCTION) ? 'production' : 'development')
+    })
   ]
 }
+
+if (process.env.PRODUCTION) {
+  config.devtool = "source-map";
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      drop_console: false,
+    },
+    sourceMap: true
+  }));
+} else {
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  );
+}
+
+console.log(config.plugins);
+
+module.exports = config;
